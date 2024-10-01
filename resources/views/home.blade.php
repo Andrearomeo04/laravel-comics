539325cc2264574169aafc5bade5cc253b2e5cc7
andrearomeo04 <pisco004@gmail.com>
2024-10-01 17:18:27 +0200

    [UPD] created placeholder in app.blade.php and created structure for jumbo section

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbo">
+        <div class="container">
+            <div class="row"></div>
+        </div>
+    </div>
+@endsection
