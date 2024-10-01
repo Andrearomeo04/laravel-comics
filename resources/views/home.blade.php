01ec1b7fbb394e4e5c94e3a036446be26b5edc30
andrearomeo04 <pisco004@gmail.com>
2024-10-02 01:40:33 +0200

    [UPD] created header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+<!-- jumbo section -->
     <div class="jumbo">
         <div class="container">
             <div class="row"></div>
+        </div>
+    </div>
+
+    <!-- comics section -->
+    <div class="container">
+        <div class="row">
         </div>
     </div>
 @endsection
